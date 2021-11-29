37eb096279968b950facd40d1163270524e75d95
cvpna <vladriordan@gmail.com>
2021-11-29 05:22:26 -0800

    1stage

---
+++ app/src/main/java/com/example/cvpna/FileManagerFragment.java
@@ -0,0 +1,24 @@
+package com.example.cvpna;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class FileManagerFragment extends Fragment {
+
+    public FileManagerFragment() {
+    }
+
+    public static VPNClientFragment newInstance() {
+        return new VPNClientFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_filemanager, container, false);
+    }
+}
--- app/src/main/java/com/example/cvpna/MainActivity.java
+++ app/src/main/java/com/example/cvpna/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.cvpna;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_vpnclient:
+                    loadFragment(VPNClientFragment.newInstance());
+                    return true;
+                case R.id.navigation_filemanager:
+                    loadFragment(FileManagerFragment.newInstance());
+                    return true;
+                case R.id.navigation_rdpclient:
+                    loadFragment(RDPClientFragment.newInstance());
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fl_content, fragment);
+        ft.commit();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 }
